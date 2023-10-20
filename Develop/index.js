089f493e715c1b4e930c24c147f8b9ce961d6533
jbrowning824 <31068426+jbrowning824@users.noreply.github.com>
2023-10-20 15:56:04 -0400

    initial setup of project

---
+++ Develop/index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        input: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?'
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
