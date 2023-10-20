32b4a8cce51a62f2b0dfedd7bde09eab26c17156
Joseph Browning <j.browning527@gmail.com>
2023-10-20 18:31:46 -0400

    Updated questions in questions array

--- Develop/index.js
+++ Develop/index.js
@@ -33,6 +33,18 @@ const questions = [
         message: 'How do others test your project?'
     },
     {
+        type: 'list',
+        name: 'license',
+        message: 'Select a license',
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License',
+                    'BSD 2-Clause \""Simplified\"" License', 
+                    'BSD 3-Clause \""New\"" or \""Revised\"" License', 'Boost Software License 1.0',
+                    'Creative Commons Zero v1.0', 'Eclipse Publice License 2.0', 
+                    'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1',
+                    'Mozilla Public License 2.0', 'The Unlicense'],
+        default: 'None',
+    },
+    {
         type: 'input',
         name: 'github',
         message: 'What is your github username?'
