91ba0d3c7c53f0b4f520a906f2d3cb75d5a3eec9
Joseph Browning <j.browning527@gmail.com>
2023-10-20 18:42:23 -0400

    added inquirer prompt to init()

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -57,10 +59,17 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            console.log(answers);
+        });
+}
 
 // Function call to initialize app
 init();
