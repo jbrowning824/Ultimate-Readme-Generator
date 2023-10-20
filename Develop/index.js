046bdaee467eef915d5be1251809bf3b0edf6d0c
Joseph Browning <j.browning527@gmail.com>
2023-10-20 18:18:27 -0400

    Added questions to questions array

--- Develop/index.js
+++ Develop/index.js
@@ -3,9 +3,44 @@
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        input: 'input',
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Give a brief discription of what your project does.'
+    },
+    {
+        type: 'input',
         name: 'installation',
         message: 'What are the steps required to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How should people use your project'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How should people contribute to your project?'
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'How do others test your project?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
     }
 ];
 
